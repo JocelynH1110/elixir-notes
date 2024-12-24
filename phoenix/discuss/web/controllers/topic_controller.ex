c94b8a980ba7f2a88689ec80d20c4837fd403424
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-12-24 09:49:55 +0800

    Phoenix's conn struct

--- phoenix/discuss/web/controllers/topic_controller.ex
+++ phoenix/discuss/web/controllers/topic_controller.ex
@@ -1,6 +1,12 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
-  def new() do
-   
+
+  # 這裡的 params 用來幫助我們解析 URL
+  def new(conn, params) do
+    IO.puts "++++"  # 輸出一些簡單的字串用 puts
+    IO.inspect conn # 輸出一些深度嵌套資料結構用 inspect。inspect 會爬遍所有的資料結構，試著印出所有的值。
+    IO.puts "++++"  
+    IO.inspect params
+    IO.puts "++++"  
   end
 end
