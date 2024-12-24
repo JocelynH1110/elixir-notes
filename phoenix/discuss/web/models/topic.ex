c23af5b23d8d604ee1ac089e8cd015537c4b122a
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-12-24 20:12:57 +0800

    Model on changesets validations

--- phoenix/discuss/web/models/topic.ex
+++ phoenix/discuss/web/models/topic.ex
@@ -8,7 +8,8 @@ defmodule Discuss.Topic do
   end
 
   # struct 代表在資料庫裡的紀錄；params 代表我們想更新的
-  def changeset(struct, params \\ %{}) do
+  # // 做了預設值引數在 elixir，如果傳送一個 nil 給 params，它將被預設為 empty map
+  def changeset(struct, params \\ %{}) do 
     struct
     |> cast(params, [:title]) # 產生一個 changeset，裡面描述如何將原有的 struct，更新成 pramas
     |> validate_required([:title])
