cc9c7e63601b21b480fbc71eb99a9622c6b9c010
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2024-12-26 18:04:40 +0800

    Repo modele

--- phoenix/discuss/web/controllers/topic_controller.ex
+++ phoenix/discuss/web/controllers/topic_controller.ex
@@ -4,7 +4,7 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic # 將下面原本要寫成 Discuss.Topic 縮短成 Topic
 
   # 這裡的 params 用來幫助我們解析 URL
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     # show the new template
@@ -12,6 +12,16 @@ defmodule Discuss.TopicController do
     render conn, "new.html", changeset: changeset
   end
 
+  # 將表格裡的資料傳到第二個引數
+  # 第二引數值是由 params = %{"topic" = "使用者輸入的資料"}，轉化而來，因為 params.topic 無法直接找到值，因為 key 是字串，所以改成 %{"topic" => topic} = params 用 topic 來接收 "使用者輸入的資料"。
   def create(conn, %{"topic" => topic}) do
+     changeset = Topic.changeset(%Topic{}, topic)
+
+    # 將新增的資料插入資料庫
+    # insert 會自動探測 changeset 是否有符合
+    case Repo.insert(changeset) do
+    {:ok, post} ->IO.inspect(post) 
+    {:error, changeset} ->IO.inspect(changeset)
+    end
   end
 end
