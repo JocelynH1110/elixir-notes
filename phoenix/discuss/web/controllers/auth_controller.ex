e410eba51062a1212ae87dff3c27040358ac883f
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-01-07 18:07:07 +0800

    Defining OAuth routes

---
+++ phoenix/discuss/web/controllers/auth_controller.ex
@@ -0,0 +1,13 @@
+defmodule Discuss.AuthController do
+  # use 部份是告訴 phoenix 配置這個 module 當成 controller
+  use Discuss.Web, :controller
+  plug Ueberauth
+
+  def callback(conn, params) do
+    IO.puts "+++++"
+    IO.inspect(conn, assigns)
+    IO.puts "+++++"
+    IO.inspect(params)
+    IO.puts "+++++"
+  end
+end
--- phoenix/discuss/web/router.ex
+++ phoenix/discuss/web/router.ex
@@ -20,6 +20,18 @@ defmodule Discuss.Router do
     resources "/", TopicController
   end
 
+  scope "/auth", Discuss do
+    pipe_through :browser # Use the default browser stack
+
+    # :request 自動被 plug Ueberauth module 定義了，所以我們不用定義。
+    # 將原本 "/github" 改寫成 "/:provider" ，這樣就不單單只可以接收一種 route
+    # 這一個是處理剛進來初始的 OAuth 要求
+    get "/:provider", AuthController, :request   
+
+    # 這一個處理從 github 返回的
+    get "/:provider/callback", AuthController, :callback
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", Discuss do
   #   pipe_through :api
