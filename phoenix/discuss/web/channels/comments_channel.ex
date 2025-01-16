0ee8e7f437aad3f490446ba24d60855652240d9b
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-01-16 18:08:28 +0800

    Fetching topics on join()

--- phoenix/discuss/web/channels/comments_channel.ex
+++ phoenix/discuss/web/channels/comments_channel.ex
@@ -1,13 +1,18 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
+  alias Discuss.Topic
+
   @doc """
     當使用者進入 channel，會寄送所有與目前 topic 有關的 comments。
   """
-  # name 是 socket.js 裡 let channel 的第一個參數，socker 代表 socket connection
-  def join(name, _params, socket) do
-    IO.puts(name)
-    {:ok, %{hey: "there"}, socket}
+  # 第一個參數是 socket.js 裡 let channel 的第一個參數，socker 代表 socket connection
+  # <> 是如何把 string 在 elixir 加在一起。無論 comments: 後接了什麼字，都會自動被安排進變數 topic_id 裡。
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)  # 因為取得的是字串要改成 psql、ecto 認為的 int
+    topic = Repo.get(Topic, topic_id) # 從 Topic models 裡拿出 topic_id
+
+    {:ok, %{}, socket}
   end
 
   @doc """
