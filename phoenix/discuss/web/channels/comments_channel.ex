f8ca93786998e351377e1016f9a31e7e477f3d43
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-01-17 10:28:57 +0800

    Inserting comments into the database

--- phoenix/discuss/web/channels/comments_channel.ex
+++ phoenix/discuss/web/channels/comments_channel.ex
@@ -1,7 +1,7 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  alias Discuss.Topic
+  alias Discuss.{Topic, Comment}
 
   @doc """
     當使用者進入 channel，會寄送所有與目前 topic 有關的 comments。
@@ -12,13 +12,24 @@ defmodule Discuss.CommentsChannel do
     topic_id = String.to_integer(topic_id)  # 因為取得的是字串要改成 psql、ecto 認為的 int
     topic = Repo.get(Topic, topic_id) # 從 Topic models 裡拿出 topic_id
 
-    {:ok, %{}, socket}
+    {:ok, %{}, assign(socket, :topic, topic)}   # 將 topic 的整個 struct 安排進 socket。socket.assign.topic。為了將得到的 topic_id 拿給 handle_in 使用。
   end
 
   @doc """
-    當使用者加入新的 comment，或點擊 submit， 將會發射一個事件或發布一個事件給會收到 handle_in() 的伺服器。會儲存 comment
+    當使用者加入新的 comment，或點擊 submit， 將會發射一個事件或發布一個事件給會收到 handle_in() 的伺服器。會儲存 comment 到資料庫。
   """
   def handle_in(name, %{"content" => content}, socket) do
-    {:reply, :ok, socket}   # 這是告訴 phoenix 目前狀況，讓它回覆給 user
+    topic = socket.assigns.topic
+
+    changeset = topic
+      |> build_assoc(:comments)   # comments list
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} -> 
+        {:reply, :ok, socket}   # 這是告訴 phoenix 目前狀況，讓它回覆給 user
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}    
+    end
   end
 end
