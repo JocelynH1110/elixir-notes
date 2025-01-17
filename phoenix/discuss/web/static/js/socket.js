acb8984955b85fa284942333b3b6677e5a6428ff
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-01-17 18:12:04 +0800

    Rendering a list of comments

--- phoenix/discuss/web/static/js/socket.js
+++ phoenix/discuss/web/static/js/socket.js
@@ -8,7 +8,9 @@ const createSocket = (topicId) => {
   // 會跑到 user_socket.ex 裡看有沒有 comments: 這個 channel
   let channel = socket.channel(`comments:${topicId}`, {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => { 
+      renderComments(resp.comments);
+    })
     .receive("error", resp => { console.log("Unable to join", resp) })
 
   // 取出 textarea 的值
@@ -18,6 +20,19 @@ const createSocket = (topicId) => {
     // 在 comments_channel 的 handle_in() 裡，會收到以下 push
     channel.push('comment:add', { content: content });
   });
+}
+
+function renderComments(comments){
+  const renderedComments = comments.map(comment => {
+    return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+    `;
+  });
+
+  // 找在 show.html 設定好的元素，將處理好的 comments 列出。因為取得的是 array，要把它轉為 string，故用 join('')
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
 }
 
 window.createSocket = createSocket;
